412075adbae0502ded1ba998ae4faeb2f7e6b6c8
sametbakmaz <sametbkmz@gmail.com>
2022-10-03 12:46:40 +0300

    Initial Commit

---
+++ HaberProjesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaberProjesi
+//
+//  Created by Abdulsamet Bakmaz on 3.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
